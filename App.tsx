f92f573eb6ce6202bcfc9416840c13fcde6cf23d
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-25 00:53:44 +0600

    welcome screen boilerplate

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React, { useEffect } from 'react'
 import RNBootSplash from 'react-native-bootsplash'
-import Onboarding from './src/Authentication/Onboarding'
+import { Onboarding, Welcome } from './src/Authentication'
 
 const AuthenticationStack = createStackNavigator()
 
 const AuthenticationNavigator: React.FC = () => (
   <AuthenticationStack.Navigator headerMode='none'>
     <AuthenticationStack.Screen name='Onboarding' component={Onboarding} />
+    <AuthenticationStack.Screen name='Welcome' component={Welcome} />
   </AuthenticationStack.Navigator>
 )
 
---
+++ src/Authentication/Welcome/Welcome.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+interface WelcomeProps {}
+
+const Welcome: React.FC<WelcomeProps> = () => {
+  return <View />
+}
+
+export default Welcome
