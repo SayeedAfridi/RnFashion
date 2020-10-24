d002348bd46c3a2e360f104f7fa14ccf880637ec
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-24 13:03:53 +0600

    Change package name and icons

--- android/app/src/main/java/com/imgfy/MainActivity.java
+++ android/app/src/main/java/com/imgfy/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sayeed.imgfy;
+package com.sayeed.rnfashion;
 
 import com.facebook.react.ReactActivity;
 import com.zoontek.rnbootsplash.RNBootSplash;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected String getMainComponentName() {
-    return "ImgFy";
+    return "RnFashion";
   }
 
   @Override
--- android/app/src/main/java/com/imgfy/MainApplication.java
+++ android/app/src/main/java/com/imgfy/MainApplication.java
@@ -1,4 +1,4 @@
-package com.sayeed.imgfy;
+package com.sayeed.rnfashion;
 
 import android.app.Application;
 import android.content.Context;
