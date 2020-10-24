41a7ed35a9dfdbe7207f0773d03672960c9a36e8
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-25 02:38:40 +0600

    removed color palete

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,20 +1,5 @@
 import { createTheme, createText, createBox } from '@shopify/restyle'
 
-const palette = {
-  purpleLight: '#8C6FF7',
-  purplePrimary: '#5A31F4',
-  purpleDark: '#3F22AB',
-
-  greenLight: '#56DCBA',
-  greenPrimary: '#0ECD9D',
-  greenDark: '#0A906E',
-
-  black: '#0B0B0B',
-  white: '#F0F2F3',
-  textPrimaryColor: '#0c0d34',
-  textSecondaryColor: 'rgba(12, 13, 52, 0.7)',
-}
-
 const theme = createTheme({
   colors: {
     primary: '#2cb9b0',
