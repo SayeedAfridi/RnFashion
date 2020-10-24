9c92e17559508d57c6bfd5254ee9911cefd21550
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-25 05:21:16 +0600

    navigate to login

--- src/Authentication/Welcome/Welcome.tsx
+++ src/Authentication/Welcome/Welcome.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, theme, Text, Button } from '../../components'
 import { Image, Dimensions } from 'react-native'
+import { Routes, StackNavigationProps } from '../../components/Navigation'
 
 const BORDER_RADIUS = theme.borderRadii.xl
 const { width } = Dimensions.get('window')
@@ -11,7 +12,9 @@ const image = {
   height: 575,
 }
 
-const Welcome = () => {
+const Welcome: React.FC = ({
+  navigation,
+}: StackNavigationProps<Routes, 'Welcome'>) => {
   return (
     <Box flex={1} backgroundColor='white'>
       <Box
@@ -51,7 +54,11 @@ const Welcome = () => {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque,
             perferendis.
           </Text>
-          <Button variant='primary' label='Have an account? Login' />
+          <Button
+            variant='primary'
+            label='Have an account? Login'
+            onPress={() => navigation.navigate('Login')}
+          />
           <Button variant='default' label="Join us it's free" />
           <Button variant='transparent' label='Forgot password?' />
         </Box>
