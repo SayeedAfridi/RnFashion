6959b96eeadbe241040da29d4a9d73b66478e8b0
Sayeed Afridi <as.afridi563@gmail.com>
2020-10-25 09:39:49 +0600

    fix keyboard issue

--- src/components/Conainer.tsx
+++ src/components/Conainer.tsx
@@ -1,18 +1,19 @@
 import React, { ReactNode } from 'react'
-import { Dimensions, Image, StyleSheet } from 'react-native'
+import { Dimensions, Image, StyleSheet, ScrollView } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import theme, { Box } from './theme'
+import { Box } from './theme'
 
 interface ContainerProps {
   children: ReactNode
   footer: ReactNode
 }
 
-const { width } = Dimensions.get('window')
+const { width, height: s } = Dimensions.get('window')
 
 const aspectRatio = 2400 / 3200
 const height = width * aspectRatio
 const patterns = [require('../assets/paterns/1.png')]
+const BOX_HEIGHT = height * 0.5
 
 const Conainer: React.FC<ContainerProps> = ({
   children,
@@ -20,47 +21,49 @@ const Conainer: React.FC<ContainerProps> = ({
 }: ContainerProps) => {
   const insets = useSafeAreaInsets()
   return (
-    <Box flex={1} backgroundColor='secondary'>
-      <Box backgroundColor='white'>
-        <Box
-          borderBottomLeftRadius='xl'
-          backgroundColor='white'
-          overflow='hidden'
-          height={height * 0.61}>
+    <ScrollView>
+      <Box height={s} flex={1} backgroundColor='secondary'>
+        <Box backgroundColor='white'>
+          <Box
+            borderBottomLeftRadius='xl'
+            backgroundColor='white'
+            overflow='hidden'
+            height={BOX_HEIGHT}>
+            <Image
+              source={patterns[0]}
+              style={{
+                width,
+                height,
+              }}
+            />
+          </Box>
+        </Box>
+        <Box flex={1} overflow='hidden'>
           <Image
             source={patterns[0]}
             style={{
+              ...StyleSheet.absoluteFillObject,
               width,
               height,
+              top: -BOX_HEIGHT,
             }}
           />
+          <Box
+            flex={1}
+            backgroundColor='white'
+            borderRadius='xl'
+            borderTopLeftRadius='none'>
+            {children}
+          </Box>
         </Box>
-      </Box>
-      <Box flex={1} overflow='hidden'>
-        <Image
-          source={patterns[0]}
-          style={{
-            ...StyleSheet.absoluteFillObject,
-            width,
-            height,
-            top: -height * 0.61,
-          }}
-        />
         <Box
-          flex={1}
-          backgroundColor='white'
-          borderRadius='xl'
-          borderTopLeftRadius='none'>
-          {children}
+          backgroundColor='secondary'
+          paddingTop='m'
+          style={{ paddingBottom: insets.bottom }}>
+          {footer}
         </Box>
       </Box>
-      <Box
-        backgroundColor='secondary'
-        paddingTop='m'
-        style={{ paddingBottom: insets.bottom }}>
-        {footer}
-      </Box>
-    </Box>
+    </ScrollView>
   )
 }
 
