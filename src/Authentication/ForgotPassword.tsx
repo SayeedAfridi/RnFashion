ce4ffaa5a95b9a17c93d2a4d3c05c2ad803978b8
Sayeed Afridi <as.afridi563@gmail.com>
2020-11-01 04:50:58 +0600

    forgot pass

---
+++ src/Authentication/ForgotPassword.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Button, Container, Text, Box, TextInput } from '../components'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import Footer from './components/Footer'
+import { Linking } from 'react-native'
+const ForgotPasswordSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+})
+
+const ForgotPassword = () => {
+  const { handleChange, handleBlur, handleSubmit, errors, touched } = useFormik(
+    {
+      initialValues: { email: '' },
+      validationSchema: ForgotPasswordSchema,
+      onSubmit: (val) => console.log(val),
+    },
+  )
+  const footer = (
+    <Footer
+      title="Don't work?"
+      action='Try another way.'
+      onPress={() => Linking.openURL('https://google.com')}
+    />
+  )
+  return (
+    <Container footer={footer}>
+      <Box padding='xl' paddingBottom='s'>
+        <Text variant='title1' textAlign='center'>
+          Forgot Password?
+        </Text>
+        <Text variant='body' textAlign='center'>
+          Enter the email address associated with your account.
+        </Text>
+      </Box>
+      <Box alignItems='center' justifyContent='center' padding='m'>
+        <TextInput
+          onChangeText={handleChange('email')}
+          icon='mail'
+          onBlur={handleBlur('email')}
+          placeholder='Enter your Email'
+          error={errors.email}
+          touched={touched.email}
+          autoCompleteType='email'
+          keyboardType='email-address'
+          autoCapitalize='none'
+          returnKeyType='go'
+          returnKeyLabel='Go'
+          onSubmitEditing={() => handleSubmit()}
+        />
+        <Box height={10} />
+        <Button
+          onPress={handleSubmit}
+          variant='primary'
+          label='Reset password'
+        />
+      </Box>
+    </Container>
+  )
+}
+
+export default ForgotPassword
